39dcb259ccb1641908b4582c3f16d72c521bc2f1
tndreka <tndreka@student.42heilbronn.de>
2025-08-09 15:57:17 +0200

    ADD: init the header file

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/08 17:01:36 by tndreka           #+#    #+#             */
+/*   Updated: 2025/08/09 15:56:23 by tndreka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+
+class ScalarConverter
+{
+private:
+	ScalarConverter();
+	ScalarConverter(const ScalarConverter& other);
+	ScalarConverter& operator=(const ScalarConverter& other);
+	~ScalarConverter();
+public:
+	static void convert(const std::string& str);
+};
+
+#endif
