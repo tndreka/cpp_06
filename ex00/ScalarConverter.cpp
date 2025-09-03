b017608267f3af7fe99746f58b270d904922b405
tndreka <tndreka@student.42heilbronn.de>
2025-09-03 13:42:46 +0200

    ADD: created Makefile, created ScalarConverter header file with one static convert function that acceptss string literals as a parameter, in .cpp file implemented the canonical form and the convert function/

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,40 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 17:01:47 by tndreka           #+#    #+#             */
-/*   Updated: 2025/08/08 17:01:48 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/08/09 16:51:49 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter()
+{}
+
+ScalarConverter::ScalarConverter(const ScalarConverter& other)
+{
+	(void)other;
+}
+
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other)
+{
+	(void)other;
+	return *this;
+}
+
+ScalarConverter::~ScalarConverter()
+{}
+
+//Casting in C++
+/*
+	static_cast<type>(data);
+	this is the excplicit cast operator that converts expression in compile time.
+*/
+
+void ScalarConverter::convert(const std::string& str)
+{
+	//Char
+	if(str.length() == 1 && isalpha(static_cast<unsigned char>(str[0])))
+		std::cout << "Char: " << str << std::endl;
+	else
+		std::cout << "The input is: " << str << std::endl;
+}
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,21 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 16:44:04 by tndreka           #+#    #+#             */
-/*   Updated: 2025/08/08 16:44:05 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/08/09 16:52:45 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScalarConverter.hpp"
+
+
+int main(int ac, char *av[])
+{
+	if (ac == 1)
+	{
+		std::cout << "ERROR: Wrong number of arguments" <<std::endl;
+		std::cout << "Try: <./Program_NAME> <INPUT>" <<std::endl;
+		return 1;
+	}
+	ScalarConverter::convert(av[1]);
+	return 0;
+}
