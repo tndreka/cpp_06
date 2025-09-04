bf34932c64823d536d6a335d48f2d39af9e055ee
tndreka <tndreka@student.42heilbronn.de>
2025-09-04 17:00:14 +0200

    ADD: implemented the serialize function and used reintepret_cast<> so the conversion from a pointer to an uintptr_t is successful and also tested in the int main. now to do deserialize

--- ex01/Serializer.cpp
+++ ex01/Serializer.cpp
@@ -6,7 +6,7 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 23:42:25 by tndreka           #+#    #+#             */
-/*   Updated: 2025/09/04 16:42:25 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/09/04 16:50:14 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,20 @@ Serializer& Serializer::operator=(const Serializer& other)
 Serializer::~Serializer(){}
 
 /*
-
+	reinterpret_cast<data_type>
+	- this cast is used when you want to reinterpret the binary bits of a value
+	as another type.
+	- it does not change the actual bit but tells the compiler to treat those bits as if they 
+	were a different type without losing information in the conversion. 
+	- it uses uintptr_t because the that is capable of holding a pointer.
 */
 uintptr_t Serializer::serialize(Data *ptr)
 {
-	
+	return reinterpret_cast<uintptr_t>(ptr);
 }
 
-/*
-*/
-Data* Serializer::deserialize(uintptr_t raw)
-{
+
+// Data* Serializer::deserialize(uintptr_t raw)
+// {
 	
-}
+// }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,25 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 23:39:37 by tndreka           #+#    #+#             */
-/*   Updated: 2025/09/03 23:41:14 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/09/04 16:58:48 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Serializer.hpp"
+
+int main()
+{
+
+	std::cout << "=============== TEST 1 ==================== " << std::endl;
+	Data myptr;
+	myptr.id = 1;
+	myptr.name = "taulant";
+	Data *ptr = &myptr;
+	
+	uintptr_t raw = Serializer::serialize(ptr);
+	std::cout << "pointer uintptr_t value: " << raw << std::endl;
+	
+	std::cout << "=============== TEST 2 ==================== " << std::endl;
+	
+	return 0;
+}
