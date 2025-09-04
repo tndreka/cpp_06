cc66d168293bd30ab74134e169458c3e9e81844b
tndreka <tndreka@student.42heilbronn.de>
2025-09-04 16:16:12 +0200

    ADD: created the mandatory files

---
+++ ex01/Serializer.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/03 23:42:07 by tndreka           #+#    #+#             */
+/*   Updated: 2025/09/04 16:15:14 by tndreka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERIALIZER_HPP
+#define SERIALIZER_HPP
+
+class Serializer
+{
+private:
+
+	Serializer();
+	Serializer(const Serializer& other);
+	Serializer& operator=(const Serializer& other);
+	~Serializer();
+public:
+};
+
+#endif
