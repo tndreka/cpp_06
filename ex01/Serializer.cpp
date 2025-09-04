62ba94a316fdf5b9a49bba0f495b81a981db892d
tndreka <tndreka@student.42heilbronn.de>
2025-09-04 16:43:36 +0200

    ADD: created the header file with the DATA struct, in .cpp implemented the canonical form and now to do the two main functions

--- ex01/Serializer.cpp
+++ ex01/Serializer.cpp
@@ -6,7 +6,38 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 23:42:25 by tndreka           #+#    #+#             */
-/*   Updated: 2025/09/03 23:42:26 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/09/04 16:42:25 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Serializer.hpp"
+
+Serializer::Serializer(){}
+
+Serializer::Serializer(const Serializer& other)
+{
+	(void)other;
+}
+
+Serializer& Serializer::operator=(const Serializer& other)
+{
+	(void)other;
+	return *this;
+}
+
+Serializer::~Serializer(){}
+
+/*
+
+*/
+uintptr_t Serializer::serialize(Data *ptr)
+{
+	
+}
+
+/*
+*/
+Data* Serializer::deserialize(uintptr_t raw)
+{
+	
+}
--- ex01/Serializer.hpp
+++ ex01/Serializer.hpp
@@ -6,22 +6,37 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 23:42:07 by tndreka           #+#    #+#             */
-/*   Updated: 2025/09/04 16:15:14 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/09/04 16:29:16 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERIALIZER_HPP
 #define SERIALIZER_HPP
 
+#include <iostream>
+#include <string>
+#include <cstdint> // for uintptr_t
+
+/*
+	Data struct
+*/
+struct Data
+{
+	std::string name;
+	int id;
+};
+
+
 class Serializer
 {
-private:
-
-	Serializer();
-	Serializer(const Serializer& other);
-	Serializer& operator=(const Serializer& other);
-	~Serializer();
-public:
+	private:
+		Serializer();
+		Serializer(const Serializer& other);
+		Serializer& operator=(const Serializer& other);
+		~Serializer();
+	public:
+		static uintptr_t serialize(Data* ptr);
+		static Data* deserialize(uintptr_t raw);
 };
 
 #endif
